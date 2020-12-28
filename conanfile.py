300e2f49fc650d0b7b14a0d4c0ea985155638448
Till Heinzel <till@columno.com>
2020-12-28 08:27:42 +0100

    update version

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,14 @@ import re
 
 class MongoCxxConan(ConanFile):
     name = "mongo-cxx-driver"
-    version = "3.3.0"
+    version = "3.6.1"
     url = "http://github.com/bincrafters/conan-mongo-cxx-driver"
     description = "C++ Driver for MongoDB"
     license = "https://github.com/mongodb/mongo-cxx-driver/blob/{0}/LICENSE".format(version)
     settings =  "os", "compiler", "arch", "build_type"
     options = {"shared": [True, False]}
     default_options = {'shared': 'False'}
-    requires = 'mongo-c-driver/[~=1.9]@bincrafters/stable'
+    requires = 'mongo-c-driver/[~=1.17]'
     generators = "cmake"
 
     def source(self):
@@ -23,44 +23,24 @@ class MongoCxxConan(ConanFile):
     def build(self):
         conan_magic_lines='''project(MONGO_CXX_DRIVER LANGUAGES CXX)
         include(../conanbuildinfo.cmake)
-        conan_basic_setup()
-        # include(CheckCXXCompilerFlag)
-        # check_cxx_compiler_flag(-std=c++17 HAVE_FLAG_STD_CXX17)
-        # if(HAVE_FLAG_STD_CXX17)
-        #     message(STATUS USING C++17!)
-        #     set(CMAKE_CXX_STANDARD 17)
-        #     set(BSONCXX_POLY_USE_MNMLSTC 0)
-        #     set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 0)
-        #     set(BSONCXX_POLY_USE_BOOST 0)
-        #     set(BSONCXX_POLY_USE_STD 1)
-        # else()
-        #     check_cxx_compiler_flag(-std=c++14 HAVE_FLAG_STD_CXX14)
-        #     if(HAVE_FLAG_STD_CXX14)
-        #         message(STATUS USING C++14!)
-        #         set(CMAKE_CXX_STANDARD 14)
-        #         set(BSONCXX_POLY_USE_MNMLSTC 0)
-        #         set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 1)
-        #         set(BSONCXX_POLY_USE_BOOST 0)
-        #         set(BSONCXX_POLY_USE_STD 0)
-        #     else()
-        #         message(FATAL_ERROR "MongoCXX requires at least C++14")
-        #     endif()
-        # endif()
-        # set(CMAKE_CXX_STANDARD_REQUIRED ON)
-        # set(CMAKE_CXX_EXTENSIONS OFF)
-        set(BSONCXX_POLY_USE_MNMLSTC 1)
-        set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 0)
-        set(BSONCXX_POLY_USE_BOOST 0)
-        set(BSONCXX_POLY_USE_STD 0)
-        '''
+        conan_basic_setup()  
         
+        '''
+        if self.settings.compiler == 'Visual Studio':
+                conan_magic_lines += "add_definitions(-D_ENABLE_EXTENDED_ALIGNED_STORAGE)\n"
+                conan_magic_lines += "add_definitions(-D_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING)\n"
+                conan_magic_lines += "add_compile_options(/Zc:__cplusplus)\n"
+        elif str(self.settings.compiler) in ["gcc", "clang"]:
+                conan_magic_lines += "add_definitions(-Wno-deprecated-declarations)"
+
         cmake_file = "sources/CMakeLists.txt"
         tools.replace_in_file(cmake_file, "project(MONGO_CXX_DRIVER LANGUAGES CXX)", conan_magic_lines)
         content = tools.load(cmake_file)
 
         cmake = CMake(self)
-        if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["BSONCXX_POLY_USE_BOOST"] = 1 # required for Windows.
+        cmake.definitions["CMAKE_CXX_STANDARD"]=17
+        cmake.definitions["BSONCXX_POLY_USE_STD"]=True
+        cmake.definitions["BUILD_VERSION"]=self.version
         cmake.configure(source_dir="sources")
         cmake.build()
 
@@ -92,14 +72,14 @@ class MongoCxxConan(ConanFile):
         except:
             pass
         try:
-            os.rename("lib/libmongocxx-static.lib", "lib/libmongocxx.lib")
+            os.rename("lib/mongocxx-static.lib", "lib/mongocxx.lib")
         except:
             pass
         try:
-            os.rename("lib/libbsoncxx-static.lib", "lib/libbsoncxx.lib")
+            os.rename("lib/bsoncxx-static.lib", "lib/bsoncxx.lib")
         except:
             pass
-        self.copy(pattern="lib*cxx.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*cxx.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="lib*cxx.a", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="lib*cxx.so*", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="lib*cxx.dylib", dst="lib", src="lib", keep_path=False)
